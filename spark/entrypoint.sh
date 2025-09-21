f8f45f3683df51079ccc5ecdbad742cb95e3e044
Idris Adebisi <idris.adebisi@yahoo.com>
2025-09-21 17:01:23 +0100

    update deployment

--- spark/entrypoint.sh
+++ spark/entrypoint.sh
@@ -8,9 +8,6 @@ if [ "$1" = "master" ]; then
 elif [ "$1" = "worker" ]; then
   echo "Starting Spark worker..."
   $SPARK_HOME/bin/spark-class org.apache.spark.deploy.worker.Worker $SPARK_MASTER_URL
-elif [ "$1" = "notebook" ]; then
-  echo "Starting notebook server..."
-  exec pyspark
 else
   echo "Running custom command: $@"
   exec "$@"

e4ca295cf2b74cf77ddb0c9ba90969f4c58d729a
Idris Adebisi <idris.adebisi@yahoo.com>
2025-10-10 19:12:26 +0100

    move spark scripts into airflow directory

--- airflow/dags/spark_dags.py
+++ airflow/dags/spark_dags.py
@@ -22,21 +22,19 @@ with DAG(
     start = EmptyOperator(task_id="start")
     end = EmptyOperator(task_id="end")
 
-    # --- Run Migration to create fact and dimension tables ---
     run_migrations = SparkSubmitOperator(
         task_id="run_migrations",
         name="run_migrations_v1",
-        application="/opt/airflow/scripts/migrations_v1.py",
+        application="/opt/airflow/spark/migrations_v1.py",
         conf={
             "spark.driver.memory": "512m",
         },
     )
 
-    # --- Dimension tables ---
     dim_customers = SparkSubmitOperator(
         task_id="dim_customers",
         name="dim_customers_job",
-        application="/opt/airflow/scripts/dim_customer.py",
+        application="/opt/airflow/spark/dim_customer.py",
         conf={
             "spark.driver.memory": "512m",
         },
@@ -45,7 +43,7 @@ with DAG(
     dim_sellers = SparkSubmitOperator(
         task_id="dim_sellers",
         name="dim_seller_job",
-        application="/opt/airflow/scripts/dim_seller.py",
+        application="/opt/airflow/spark/dim_seller.py",
         conf={
             "spark.driver.memory": "512m",
         },
@@ -54,7 +52,7 @@ with DAG(
     dim_products = SparkSubmitOperator(
         task_id="dim_products",
         name="dim_product_job",
-        application="/opt/airflow/scripts/dim_product.py",
+        application="/opt/airflow/spark/dim_product.py",
         conf={
             "spark.driver.memory": "512m",
         },
@@ -63,18 +61,17 @@ with DAG(
     stg_order_order_items = SparkSubmitOperator(
         task_id="stg_order_order_items",
         name="stg_order_order_item_job",
-        application="/opt/airflow/scripts/stg_order_order_item.py",
+        application="/opt/airflow/spark/stg_order_order_item.py",
         conf={
             "spark.executor.memory": "2G",
             "spark.driver.memory": "1G",
         },
     )
 
-    # --- Fact table ---
     fact_order_items = SparkSubmitOperator(
         task_id="fact_order_items",
         name="fact_order_item_job",
-        application="/opt/airflow/scripts/fact_order_items.py",
+        application="/opt/airflow/spark/fact_order_items.py",
         conf={
             "spark.executor.memory": "2G",
             "spark.executor.instances": "2",
@@ -84,14 +81,13 @@ with DAG(
     fact_order_summary = SparkSubmitOperator(
         task_id="fact_order_summary",
         name="fact_order_summary_job",
-        application="/opt/airflow/scripts/fact_order_summary.py",
+        application="/opt/airflow/spark/fact_order_summary.py",
         conf={
             "spark.executor.memory": "2G",
             "spark.executor.instances": "2",
         },
     )
 
-    # --- Set dependencies ---
     start >> run_migrations
 
     run_migrations >> [stg_order_order_items, dim_customers, dim_sellers, dim_products]

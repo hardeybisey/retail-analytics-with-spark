d8a3fe79594e35fc945e0aee24844a827a2e7957
Idris Adebisi <idris.adebisi@yahoo.com>
2025-11-01 15:10:41 +0000

    fix sql SCD-2 dimenstion query bug in (dim_seller, dim_customer, and dim_product tables

--- airflow/dags/spark_dags.py
+++ airflow/dags/spark_dags.py
@@ -4,8 +4,8 @@ import os
 from airflow.providers.standard.operators.empty import EmptyOperator
 from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
 
-SPARK_CONN_NAME = os.environ.get("SPARK_CONN_NAME", "spark_default")
-DEPLOY_MODE = os.environ.get("SPARK_DEPLOY_MODE", "client")
+SPARK_CONN_NAME = os.environ["SPARK_CONN_NAME"]
+DEPLOY_MODE = os.environ["SPARK_DEPLOY_MODE"]
 
 
 with DAG(
@@ -21,6 +21,7 @@ with DAG(
 ) as dag:
     start = EmptyOperator(task_id="start")
     end = EmptyOperator(task_id="end")
+
     run_migrations = SparkSubmitOperator(
         task_id="run_migrations",
         name="run_migrations_v1",
@@ -38,76 +39,75 @@ with DAG(
             "spark.driver.memory": "512m",
         },
     )
-    start >> run_migrations >> dim_customers >> end
 
-    # dim_sellers = SparkSubmitOperator(
-    #     task_id="dim_sellers",
-    #     name="dim_seller_job",
-    #     application="/opt/spark/spark_jobs/dim_seller.py",
-    #     conf={
-    #         "spark.driver.memory": "512m",
-    #     },
-    # )
+    dim_sellers = SparkSubmitOperator(
+        task_id="dim_sellers",
+        name="dim_seller_job",
+        application="/opt/spark/spark_jobs/dim_seller.py",
+        conf={
+            "spark.driver.memory": "512m",
+        },
+    )
 
-    # dim_products = SparkSubmitOperator(
-    #     task_id="dim_products",
-    #     name="dim_product_job",
-    #     application="/opt/spark/spark_jobs/dim_product.py",
-    #     conf={
-    #         "spark.driver.memory": "512m",
-    #     },
-    # )
+    dim_products = SparkSubmitOperator(
+        task_id="dim_products",
+        name="dim_product_job",
+        application="/opt/spark/spark_jobs/dim_product.py",
+        conf={
+            "spark.driver.memory": "512m",
+        },
+    )
 
-    # stg_orders = SparkSubmitOperator(
-    #     task_id="stg_orders",
-    #     name="stg_orders_job",
-    #     application="/opt/spark/spark_jobs/stg_orders.py",
-    #     conf={
-    #         "spark.executor.memory": "1G",
-    #         "spark.driver.memory": "1G",
-    #     },
-    # )
+    stg_orders = SparkSubmitOperator(
+        task_id="stg_orders",
+        name="stg_orders_job",
+        application="/opt/spark/spark_jobs/stg_orders.py",
+        conf={
+            "spark.executor.memory": "1G",
+            "spark.driver.memory": "1G",
+        },
+    )
 
-    # stg_order_items = SparkSubmitOperator(
-    #     task_id="stg_order_items",
-    #     name="stg_order_items_job",
-    #     application="/opt/spark/spark_jobs/stg_order_items.py",
-    #     conf={
-    #         "spark.executor.memory": "1G",
-    #         "spark.driver.memory": "1G",
-    #     },
-    # )
+    stg_order_items = SparkSubmitOperator(
+        task_id="stg_order_items",
+        name="stg_order_items_job",
+        application="/opt/spark/spark_jobs/stg_order_items.py",
+        conf={
+            "spark.executor.memory": "1G",
+            "spark.driver.memory": "1G",
+        },
+    )
 
-    # fact_order_items = SparkSubmitOperator(
-    #     task_id="fact_order_items",
-    #     name="fact_order_item_job",
-    #     application="/opt/spark/spark_jobs/fact_order_items.py",
-    #     conf={
-    #         "spark.executor.memory": "2G",
-    #         "spark.executor.instances": "2",
-    #     },
-    # )
+    fact_order_items = SparkSubmitOperator(
+        task_id="fact_order_items",
+        name="fact_order_item_job",
+        application="/opt/spark/spark_jobs/fact_order_items.py",
+        conf={
+            "spark.executor.memory": "2G",
+            "spark.executor.instances": "2",
+        },
+    )
 
-    # fact_order_summary = SparkSubmitOperator(
-    #     task_id="fact_order_summary",
-    #     name="fact_order_summary_job",
-    #     application="/opt/spark/spark_jobs/fact_order_summary.py",
-    #     conf={
-    #         "spark.executor.memory": "2G",
-    #         "spark.executor.instances": "2",
-    #     },
-    # )
+    fact_order_summary = SparkSubmitOperator(
+        task_id="fact_order_summary",
+        name="fact_order_summary_job",
+        application="/opt/spark/spark_jobs/fact_order_summary.py",
+        conf={
+            "spark.executor.memory": "2G",
+            "spark.executor.instances": "2",
+        },
+    )
 
-    # start >> run_migrations
+    start >> run_migrations
 
-    # run_migrations >> [
-    #     stg_orders,
-    #     stg_order_items,
-    #     dim_customers,
-    #     dim_sellers,
-    #     dim_products,
-    # ]
+    run_migrations >> [
+        stg_orders,
+        stg_order_items,
+        dim_customers,
+        dim_sellers,
+        dim_products,
+    ]
 
-    # [dim_sellers, dim_products, stg_orders, stg_order_items] >> fact_order_items >> end
+    [dim_sellers, dim_products, stg_orders, stg_order_items] >> fact_order_items >> end
 
-    # [dim_customers, stg_orders, stg_order_items] >> fact_order_summary >> end
+    [dim_customers, stg_orders, stg_order_items] >> fact_order_summary >> end
--- airflow/spark_jobs/dim_seller.py
+++ airflow/spark_jobs/dim_seller.py
@@ -166,8 +166,8 @@ def create_seller_dim_table(spark) -> None:
     spark.sql("""
         MERGE INTO dim_seller AS target
         USING tmp_dim_seller_view AS src
-            ON target.seller_sk = src.seller_sk AND target.is_current = true
-        WHEN MATCHED THEN
+            ON target.seller_sk = src.seller_sk
+        WHEN MATCHED AND target.is_current = true THEN
             UPDATE SET
                 target.effective_to = src.effective_to,
                 target.is_current = src.is_current
